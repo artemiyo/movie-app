240804075c5e0a00f2286cadefec33cd7ad4fb59
Artem <artemiyo.dev@gmail.com>
2019-10-15 23:24:58 +0300

    Update Loader.jsx

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -12,8 +12,7 @@ const roller = keyframes`
 
 const LoaderWrapper = styled.div`
   width: 100%;
-  height: 100vh;
-  position: fixed;
+  position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
